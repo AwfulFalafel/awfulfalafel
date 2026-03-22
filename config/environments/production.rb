38ddad870a440b3001a5360c2d5ca2776db3f929
flanger001 <1645017+flanger001@users.noreply.github.com>
2026-03-22 10:24:56 -0400

    Update production hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,8 +68,7 @@ Rails.application.configure do
 	# Skip DNS rebinding protection for the default health check endpoint.
 	# config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
 	config.hosts = [
-		"awfulfalafel.band",
-		"www.awfulfalafel.band",
+		/.*\.awfulfalafel\.band/,
 		"awful.falafel.band",
 		"192.168.2.167"
 	]
